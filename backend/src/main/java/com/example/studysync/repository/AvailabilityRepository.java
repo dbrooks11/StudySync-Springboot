94db86e74cd3c059dd1aef8267b1e338348e26b7
Daivion Brooks <daivionbrooks11@gmail.com>
2026-06-14 18:22:49 -0400

    feature: custom repo queries; - added findbyId query that uses studentDTO to filter data; - added Availability listing

--- backend/src/main/java/com/example/studysync/repository/AvailabilityRepository.java
+++ backend/src/main/java/com/example/studysync/repository/AvailabilityRepository.java
@@ -3,6 +3,9 @@ package com.example.studysync.repository;
 import com.example.studysync.models.Availability;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
+    List<Availability> findAvailabilityByStudent_Id(Long studentId);
 
 }
--- backend/src/main/java/com/example/studysync/repository/StudentRepository.java
+++ backend/src/main/java/com/example/studysync/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.example.studysync.repository;
 
+import com.example.studysync.dto.StudentDTO;
 import com.example.studysync.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+    Optional<Student> findByEmail(String email);
+    Optional<StudentDTO> findStudentById(Long id);
 }
