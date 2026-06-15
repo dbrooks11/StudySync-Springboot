60d4338f09c598589bb9db26fceb90da32943b31
Daivion Brooks <daivionbrooks11@gmail.com>
2026-06-15 13:51:21 -0400

    fix & feature: course and Enrollment repository; - added query to find enrollment by student id and course id in EnrollmentRepository; - fixed param typo on Native queries and column names

--- backend/src/main/java/com/example/studysync/repository/CourseRepository.java
+++ backend/src/main/java/com/example/studysync/repository/CourseRepository.java
@@ -2,7 +2,35 @@ package com.example.studysync.repository;
 
 import com.example.studysync.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    @Query(value = """
+        SELECT c.*
+        FROM Course as c
+        JOIN Enrollment as e ON e.course_id = c.id
+        JOIN Student as s ON s.id = e.student_id
+        WHERE s.id = :studentId
+        """
+    , nativeQuery = true)
+    List<Course> findEnrolledCourses(@Param("studentId") Long studentId);
+
+    @Query(value = """
+      SELECT c.*
+      FROM course as c
+      JOIN Enrollment as e ON e.course_id = c.id
+      JOIN Student as s ON s.id = e.student_id
+      WHERE s.id = :studentId AND c.code = :courseCode AND c.id = :courseId
+    """, nativeQuery = true)
+    Optional<Course> findCourseByStudentIdAndCourseCodeAndCourseId(
+            @Param("studentId") Long studentId,
+            @Param("courseCode") String code,
+            @Param("courseId") Long id
+    );
 
 }
--- backend/src/main/java/com/example/studysync/repository/EnrollmentRepository.java
+++ backend/src/main/java/com/example/studysync/repository/EnrollmentRepository.java
@@ -4,6 +4,9 @@ import com.example.studysync.models.Enrollment;
 import com.example.studysync.models.id.EnrollmentId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EnrollmentRepository extends JpaRepository<Enrollment, EnrollmentId> {
 
+    Optional<Enrollment> findEnrollmentByStudent_IdAndCourse_Id(Long studentId, Long courseId);
 }
